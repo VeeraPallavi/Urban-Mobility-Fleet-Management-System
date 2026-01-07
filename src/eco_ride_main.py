68e20982e7337a63fe62152778f57d017c63ccfd
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-01-07 14:39:17 +0530

    Polymorphism

--- src/eco_ride_main.py
+++ src/eco_ride_main.py
@@ -1,11 +1,17 @@
-from vehicle import *
+from vehicle import Vehicle, ElectricCar, ElectricScooter
 
 class EcoRideMain:
-
-    @staticmethod
-    def main():
+    def main(self):
         print("Welcome to Eco-Ride Urban Mobility System")
-    
+        car = ElectricCar("EV101", "Tesla", 80, 5)
+        scooter = ElectricScooter("EV1001", "JAWA", 60, 100)
+        
+        vehicles = [car, scooter]
+        for vehicle in vehicles:
+            self.cost = vehicle.calculate_trip_cost(10)
+            print(f"{vehicle.model} Total trip cost : {self.cost}")
+
+
 if __name__ == "__main__":
-    EcoRideMain.main()
-    
+    ecoridemain = EcoRideMain()
+    ecoridemain.main()
