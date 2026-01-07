31c99f28131a636f8f82ce758526d8d92a79fcf0
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-01-07 14:27:57 +0530

    Encapsulation

--- src/vehicle.py
+++ src/vehicle.py
@@ -2,10 +2,33 @@ class Vehicle:
     def __init__(self, vehicle_id, model, battery_percentage):
         self.vehicle_id = vehicle_id
         self.model = model
-        self.battery_percntage = battery_percentage
+        self.__battery_percentage = 0
+        self.__maintenance_status = None
+        self.__rental_price = None 
 
-    def display(self):
-        print(f"Vehicle ID : {self.vehicle_id}")
-        print(f"Vehicle Model : {self.model}")
-        print(f"Battery Percentage : {self.battery_percntage}")
-        
+        self.set_battery_percentage(battery_percentage)
+
+    def get_battery_percentage(self):
+        return self.__battery_percentage
+    
+    def set_battery_percentage(self, battery_percentage):
+        if 0<= battery_percentage <= 100:
+            self.__battery_percentage = battery_percentage
+        else:
+            print("Invalid battery Percentage . Enter valid percentage...")
+
+    def get_rental_price(self):
+        return self.__rental_price
+    
+    def set_rental_price(self, rental_price):
+        if rental_price < 0:
+            print("Rental Price should be positive")
+        self.__rental_price = rental_price
+    
+    def get_maintenance_status(self):
+        return self.__maintenance_status
+    
+    def set_maintenance_status(self, maintenance_status):
+        self.__maintenance_status = maintenance_status
+
+    
